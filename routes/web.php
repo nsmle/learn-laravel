f38ed1ea3c7b1cf548448b71e1611f589b8fe329
nsmle <fikiproductionofficial@gmail.com>
2021-12-11 16:42:25 +0700

    B.View

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'Halaman Home';
+    return view('home');
 });
 
 Route::get('/about', function () {
-    return 'Halaman About';
+    return view('about');
 });
 
 Route::get('/blog', function () {
-    return 'Halaman Blog';
+    return view('posts');
 });
